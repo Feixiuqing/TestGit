5b4f87b9a92ed79604982aa2d349695114162f8b
Feixiuqing <74244243+Feixiuqing@users.noreply.github.com>
2020-12-27 15:32:58 +0800

    first idea update

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("hello this is my first time to use git through idea to push file!");
+        System.out.println("hello this is my first time to update git through idea to push file!");
     }
 }
