47822756fe7c2eca920472488e0f9289bb817813
Feixiuqing <74244243+Feixiuqing@users.noreply.github.com>
2020-12-27 15:28:47 +0800

    first idea commit

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,5 +1,7 @@
 public class HelloGit {
     public static void main(String[] args) {
+
         System.out.println("Hello Git!");
+        System.out.println("this is my first practice for committing through idea. ");
     }
 }
